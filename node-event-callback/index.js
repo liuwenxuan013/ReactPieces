4809919ae5ab89d412c5006b7bfb9d5496faf0c6
Wenxuan Liu <wliu122@myseneca.ca>
2019-11-12 15:21:26 -0500

    Node with event & call back
    -fix buffer via toString()
    * conclusion:
    --eventEmitter.on('eventName',eventhandler) to bind event with its handlerfunction
    --eventEmitter.emit('eventName') to fire an event;

--- node-event-callback/index.js
+++ node-event-callback/index.js
@@ -7,15 +7,23 @@ var readFile = () =>
     {
         console.log('reading file...');
         if (err) return console.error(err);
-        console.log(data);
+        console.log(data.toString());
 
+    })
+    eventEmitter.emit('go_read_next');
+}
+var readJsFile = () =>
+{
+    fs.readFile('index.js', (err, data) =>
+    {
+        console.log('reading file...');
+        if (err) return console.error(err);
+        console.log(data.toString());
+        console.log('done!');
     })
 
 }
 
-
-
-
 eventEmitter.on('go_read', readFile);
-
+eventEmitter.on('go_read_next', readJsFile);
 eventEmitter.emit('go_read');
