8481b45d7b1ad2e3f0b2fcae7ae2d0df1d10b923
Wenxuan Liu <wliu122@myseneca.ca>
2019-11-13 16:10:10 -0500

    Update client.js

--- timer-app/src/client.js
+++ timer-app/src/client.js
@@ -65,13 +65,7 @@ const client =function() {
     if (response.status >= 200 && response.status < 300) {
       return response;
     }
-    else {
-      const error = new Error(`HTTP Error ${response.statusText}`);
-      error.status = response.statusText;
-      error.response = response;
-      console.log(error);
-      throw error;
-    }
+
   }
   function parseJSON(response) {
     return response.json();
