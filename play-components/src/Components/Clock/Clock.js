d1a693994cc8e30c04a53ad79493afd81d312196
Wenxuan Liu <wliu122@myseneca.ca>
2019-11-17 16:38:01 -0500

    independent clock component complete

--- play-components/src/Components/Clock/Clock.js
+++ play-components/src/Components/Clock/Clock.js
@@ -1,20 +1,29 @@
 import React from 'react';
 //import ReactDOM from 'react-dom';
 class Clock extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={date:new Date()};
+    }
+    componentDidMount(){
+        this.timerID=setInterval(()=>this.tick(),1000);
+    }
+    componentWillUnmount()
+    {
+        clearInterval(this.timerID);
+    }
+    tick(){
+        this.setState({
+            date:new Date()
+        });
+    }
     render(){
         return(
-
             <div>
-
-                <h1>it is {this.props.date.toLocaleTimeString()}</h1>
+                <h1>it is {this.state.date.toLocaleTimeString()}</h1>
             </div>
         );
     }
-
 }
-
-
-
-
 export default Clock;
 
--- play-components/src/index.js
+++ play-components/src/index.js
@@ -4,18 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Clock from './Components/Clock/Clock';
-
-function tick(){
     ReactDOM.render(
-
-        <Clock date={new Date()}/>,
+        <Clock />,
         document.getElementById('time-stamp')
     );
-
-}
-
-setInterval(tick,1000);
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
 serviceWorker.unregister();
